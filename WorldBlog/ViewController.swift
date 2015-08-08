f7d3a10350f5254c182344cc679a72f6d0fdc5d8
ニチワ システム <appengine@nichiwa-system.co.jp>
2015-08-08 16:17:00 +0900

    Initial Commit

---
+++ WorldBlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldBlog
+//
+//  Created by ニチワ システム on 2015/08/08.
+//  Copyright (c) 2015年 ニチワ システム. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
