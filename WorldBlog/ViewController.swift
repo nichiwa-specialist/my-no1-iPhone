576eb2b9706c01b0bf131f459b960b1f57bed282
ニチワ システム <appengine@nichiwa-system.co.jp>
2015-09-12 15:21:57 +0900

    2015/09/12  地図表示

--- WorldBlog/ViewController.swift
+++ WorldBlog/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import MapKit /* MapKit frameworkをimport */
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    var mapView : MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // MapView生成
+        mapView = MKMapView()
+        
+        mapView.frame = self.view.bounds
+
+        mapView.delegate = self
+        
+        // 緯度と経度の設定
+        let myLat: CLLocationDegrees = 34.66806
+        let myLon: CLLocationDegrees = 135.501122
+        let myCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLon)
+        
+        // 尺度を設定
+        let myLatDist: CLLocationDistance = 0.02
+        let myLonDist: CLLocationDistance = 0.02
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myCoordinate, myLatDist, myLonDist)
+        mapView.setRegion(myRegion, animated: true)
+        
+        // 表示タイプ設定
+        //mapView.mapType = MKMapType.Standard
+        //mapView.mapType = MKMapType.Satellite
+        mapView.mapType = MKMapType.Hybrid
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
